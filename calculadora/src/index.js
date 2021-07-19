d9ee0a9fd90a68e9f23bef03e8a28e281fa3ed43
Bruno Dias <brunodiasdevs@gmail.com>
2021-07-19 10:53:15 -0300

    novo

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Calculator/>,
   document.getElementById('root')
 );
 
