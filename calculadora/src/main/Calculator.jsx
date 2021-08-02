be295f8818f1a151a269feff894fbadab340d4b6
Bruno Dias <brunodiasdevs@gmail.com>
2021-08-02 20:21:23 -0300

    novo

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -1,5 +1,7 @@
 import React, { Component} from 'react'
 import './Calculator.css'
+import Button from '../components/Button'
+//a
 
 export default class Calculator extends Component {
     render(){
